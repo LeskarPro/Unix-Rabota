ea4b5417fe73c23025bb499119cc31da74480626
LeskarPro <kirilt1617@gmail.com>
2021-09-10 12:42:25 +0300

    New change

--- starter.py
+++ starter.py
@@ -1,3 +1,11 @@
-class HelloWorld:
-	print("Hello world")
+class HelloWolder:
+	def __init__(self):
+		self.command = 'Hello Wolrld'
+
+	def print(self):
+		print(self.command)
+
+
+hello = HelloWolder
+print(hello)
 
